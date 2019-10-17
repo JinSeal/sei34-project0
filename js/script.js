344940ba0e0a4038792533b9e9588d63ae49cec4
JinSeal <1183958@gmail.com>
2019-10-17 21:40:13 +1100

    add computer counter play function

--- js/script.js
+++ js/script.js
@@ -11,8 +11,9 @@ const joinPlayer = function(num) {
         if (ttt.players.length >= 2) {
             message = `${name} joined!  We have ${ttt.players.length} players in the game.`
         }
-        $(`#player${num}-win-counter`).text(0);
         showMsg(message);
+        $(`#player${num}-win-counter`).text(0);
+
     } else {
         const message = `Please enter your name and choose a symbol!`
         showMsg(message);}
@@ -43,17 +44,66 @@ const joinPlayer = function(num) {
                 playing = false;
                 setTimeout(playButtonToggle(), 1500);
                 $(`#player${currentPlayerIndex+1}-win-counter`).text (ttt.players[currentPlayerIndex]['winCounter']);
-                localStorage.setItem('move', '0')
-                localStorage.setItem('ttt.board', '[]')
+                $('#turn').text(`~~~Play again!~~~`).css('text-align', 'center');
+
+                localStorage.setItem('move', '0');
+                localStorage.setItem('ttt.board', '[]');
+                localStorage.setItem("playing", playing);
             } else if (moves === $('td').length-1) {
-                const message = `DRAW! How could it happen !!!`
+                const message = `DRAW!`
                 showMsg(message);
+                playing = false;
+                localStorage.setItem("playing", playing);
+                (ttt.players[currentPlayerIndex]['winCounter']);
+                $('#turn').text(`~~~Play again!~~~`).css('text-align', 'center');
             } else {
                 currentPlayerIndex = 1 - currentPlayerIndex;
                 localStorage.setItem('currentPlayerIndex', currentPlayerIndex)
                 moves +=1;
                 showCurrentPlayer();
-                localStorage.setItem('moves', moves)
+                localStorage.setItem('moves', moves);
+                return true;
+            }
+        }
+    }
+
+    const updateAIMoves = function() {
+        const symbol = ttt.players[currentPlayerIndex]['symbol'];
+        let pos;
+        if ($('#ai-dropbtn').text() === "Easy") {
+            pos = ttt.aiEasyMove(currentPlayerIndex);
+        } else if ($('#ai-dropbtn').text() === "Hard") {
+            console.log('ture');
+            pos = ttt.aiHardMove(currentPlayerIndex);
+            console.log(pos);
+        }
+
+        if (pos) {
+            $(`#r${pos[0]}c${pos[1]}`).text(symbol);
+            if (ttt.winCheck(currentPlayerIndex,symbol)) {
+                const message = `Yeah! ${ttt.players[currentPlayerIndex]['name']} wins!`
+                showMsg(message);
+                playing = false;
+                setTimeout(playButtonToggle(), 1500);
+                $(`#player${currentPlayerIndex+1}-win-counter`).text (ttt.players[currentPlayerIndex]['winCounter']);
+                $('#turn').text(`~~~Play again!~~~`).css('text-align', 'center');
+
+                localStorage.setItem('move', '0');
+                localStorage.setItem('ttt.board', '[]');
+                localStorage.setItem("playing", playing);
+            } else if (moves === $('td').length-1) {
+                const message = `DRAW!`
+                showMsg(message);
+                playing = false;
+                localStorage.setItem("playing", playing);
+                (ttt.players[currentPlayerIndex]['winCounter']);
+                $('#turn').text(`~~~Play again!~~~`).css('text-align', 'center');
+            } else {
+                currentPlayerIndex = 1 - currentPlayerIndex;
+                localStorage.setItem('currentPlayerIndex', currentPlayerIndex)
+                moves +=1;
+                showCurrentPlayer();
+                localStorage.setItem('moves', moves);
                 return true;
             }
         }
@@ -79,120 +129,187 @@ const joinPlayer = function(num) {
     const showCurrentPlayer = () => {
         const name = ttt.players[currentPlayerIndex]['name'];
         $('#turn').text(`**** ${name}'s turn ****`).css('text-align', 'center')
-
     }
 
     const chooseCanvas = (node) => {
         const text = $(node).text();
         size = text.slice(0,1);
-        $('.dropbtn').text(text);
+        $('#canvas-dropbtn').text(text);
         return size;
     }
 
-    const buildBoard = () => {
-        $('table').text("");
-        let cellSize = `${(100/size).toFixed(0)}%`;
-        for (let i = 0; i < size; i++) {
-            let $tr = $(`<tr class="row" id="r${i}"></tr>`);
-            $('table').append($tr);
-            for (let j = 0; j < size; j++) {
-                let $td = $(`<td class="column" id="r${i}c${j}"></td>`);
-                $td.css({'width':cellSize, 'height': cellSize})
-                $tr.append($td);
+    const chooseAI = (node) => {
+        const text = $(node).text();
+        if (text !== "None" && ttt.players.length > 1) {
+            if ((ttt.players[0]['name'] !== "AI" && ttt.players[1]['name'] !== "AI")) {
+                const message = `One player only.`
+                showMsg(message);
+            } else {
+                $('#ai-dropbtn').text(text);
             }
+        } else if (text !== "None") {
+            if ($('#player1-name').val()) {
+                $('#player2-name').val("AI");
+                $('#player2-symbol').val("AI");
+                joinPlayer('2')
+            }else {
+                $('#player1-name').val("AI");
+                $('#player1-symbol').val("AI")
+                joinPlayer('1')
+            }
+            singlePlayer = true;
+            $('#ai-dropbtn').text(text);
+        } else {
+            if ($('#player1-name').val() === "AI") {
+                $('#player1-name').val("");
+                $('#player1-symbol').val("");
+            } else {
+                $('#player2-name').val("");
+                $('#player2-symbol').val("");
+            }
+            ttt.rmPlayer('AI');
+            ttt.singlePlayer = false;
+            singlePlayer = false;
+            $('#ai-dropbtn').text(text);
         }
-        localStorage.setItem('size', size);
-        ttt.initBoard(size);
     }
 
-    const loadingLocalStorage = () => {
-        const message = 'Restore last game.'
-        $('.message-box').text(message);
-        $('.alert').show();
 
-        currentPlayerIndex = //() => {
-         JSON.parse(localStorage['currentPlayerIndex'])//? JSON.parse(localStorage['currentPlayerIndex']):null}
-// } JSON.parse(localStorage['currentPlayerIndex']);
-        moves = //() =>
-        JSON.parse(localStorage['moves'])//? JSON.parse(localStorage['moves']):null;
+const buildBoard = () => {
+    $('table').text("");
+    let cellSize = `${(100/size).toFixed(0)}%`;
+    for (let i = 0; i < size; i++) {
+        let $tr = $(`<tr class="row" id="r${i}"></tr>`);
+        $('table').append($tr);
+        for (let j = 0; j < size; j++) {
+            let $td = $(`<td class="column" id="r${i}c${j}"></td>`);
+            $td.css({'width':cellSize, 'height': cellSize})
+            $tr.append($td);
+        }
+    }
+    localStorage.setItem('size', size);
+    ttt.initBoard(size);
+}
 
-        playing = JSON.parse(localStorage['playing']);
-        ttt.players = JSON.parse(localStorage['ttt.players']),
-        ttt.board = JSON.parse(localStorage['ttt.board']);
-        ttt.emptySpot = JSON.parse(localStorage['ttt.emptySpot']);
-        console.log(localStorage['size']);
-        size = JSON.parse(localStorage['size']);
+const loadingLocalStorage = () => {
+    const message = 'Restore last game.'
+    $('.message-box').text(message);
+    $('.alert').show();
+
+    currentPlayerIndex =
+    JSON.parse(localStorage['currentPlayerIndex'])
+    moves = JSON.parse(localStorage['moves']);
+
+    playing = JSON.parse(localStorage['playing']);
+    ttt.players = JSON.parse(localStorage['ttt.players']);
+
+    ttt.board = JSON.parse(localStorage['ttt.board']);
+    size = JSON.parse(localStorage['size']);
+    msgBox = localStorage['msgBox'];
+    $('#canvas-dropbtn').text(`${size} X ${size}`);
 
 
+    if (ttt.players.length === 1) {
+        const num = ttt.players[0]['index'];
+        $(`#player${num}-name`).val(ttt.players[0]['name'])
+        $(`#player${num}-symbol`).val(ttt.players[0]['symbol']);
+        $(`#player${num}-win-counter`).text(ttt.players[0]['winCounter']);
+    } else if (ttt.players.length === 2){
         $('#player1-name').val(ttt.players[0]['name'])
         $(`#player1-symbol`).val(ttt.players[0]['symbol']);
         $(`#player1-win-counter`).text(ttt.players[0]['winCounter']);
         $('#player2-name').val(ttt.players[1]['name'])
         $(`#player2-symbol`).val(ttt.players[1]['symbol']);
         $(`#player2-win-counter`).text(ttt.players[1]['winCounter']);
+    }
 
-        if (playing) {
-            buildBoard(size);
-            for (let p = 0; p < ttt.players.length; p++) {
-                for (let i = 0; i < size; i++) {
-                    for (let j = 0; j < size; j++) {
-                        if (ttt.board[i][j]=== ttt.players[p]['symbol']) {
-                            $(`#r${i}c${j}`).text($(`#player${p+1}-symbol`).val());
-                        }
+
+    if (playing) {
+        buildBoard(size);
+        $('#turn').text(`**** ${ttt.players[currentPlayerIndex]['name']}'s turn ****`);
+        for (let p = 0; p < ttt.players.length; p++) {
+            for (let i = 0; i < size; i++) {
+                for (let j = 0; j < size; j++) {
+                    if (ttt.board[i][j]=== ttt.players[p]['symbol']) {
+                        $(`#r${i}c${j}`).text($(`#player${p+1}-symbol`).val());
                     }
                 }
             }
         }
+    } else {
+        $('#turn').text(`~~~Play again!~~~`);
+    }
+}
+
+
+let currentPlayerIndex;
+let moves =0;
+let playing = false;
+let size = 3;
+let singlePlayer = false;
+
+
+
+const setup = function() {
+    if (localStorage.length) {
+        loadingLocalStorage();
     }
 
-    let currentPlayerIndex;
-    let moves =0;
-    let playing = false;
-    let size = 3;
+    playButtonToggle();
 
-
-
-
-    const setup = function() {
-        if (localStorage.length) {
-            loadingLocalStorage();
-        }
-        // getLocalStorage();
-        // displayLocalStorage();
-
+    $('#play1join').click(() => joinPlayer('1'));
+    $('#play2join').click(() => joinPlayer('2'));
+    $('#play1leave').click(() => removePlayer('1'));
+    $('#play2leave').click(() => removePlayer('2'));
+    $('#playbutton').click(() => {
+        resetBoard();
+        buildBoard();
+        currentPlayerIndex = ttt.playOrder()
+        localStorage.setItem('currentPlayerIndex', currentPlayerIndex);
+        playing = true;
+        localStorage.setItem("playing", playing);
         playButtonToggle();
-
-        $('#play1join').click(() => joinPlayer('1'));
-        $('#play2join').click(() => joinPlayer('2'));
-        $('#play1leave').click(() => removePlayer('1'));
-        $('#play2leave').click(() => removePlayer('2'));
-        $('#playbutton').click(() => {
-            resetBoard();
-            buildBoard();
-            currentPlayerIndex = ttt.playOrder()
-            playing = true;
-            localStorage.setItem("playing", playing);
-            playButtonToggle();
-            const message = `Let's play!  ${ttt.players[currentPlayerIndex]['name']} goes first.`
-            showMsg(message);
-        });
+        const message = `Let's play!  ${ttt.players[currentPlayerIndex]['name']} goes first.`
+        showMsg(message);
+    });
 
 
-        $('#alert-button').click(() => $('.alert').fadeOut(1000));
+    $('#alert-button').click(() => $('.alert').fadeOut(1000));
 
-        $('a').click((event)=>
-        chooseCanvas(event.target));
+    $('#canvas-content a').click((event)=>
+    chooseCanvas(event.target));
 
-        $(document).on('click', 'td', (event) => updateMoves(event.target));
+    $('#ai-content a').click((event)=>
+    chooseAI(event.target));
 
-    }
+    $('.memory').click(() =>{
+        localStorage.clear();
+        const message = `Local storage cleared.`;
+        showMsg(message);
+    });
+
+    $(document).on('click', 'td', (event) => {
+        if (singlePlayer) {
+            if (ttt.players[currentPlayerIndex]['name'] === "AI" && ttt.emptySpots.length === $('td').length) {
+                updateAIMoves();
+            } else {
+                updateMoves(event.target);
+                updateAIMoves();
+            }
+
+        } else {
+            updateMoves(event.target);
+        }});
 
 
-    // to do list:
-    // token with picture
-    // online game
-    // AI moves
-    // readme.md
+
+}
 
 
-    $(document).ready(setup);
+// to do list:
+// token with picture
+// online game
+// readme.md
+
+
+$(document).ready(setup);
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -12,7 +12,9 @@ const ttt = {
 
     players: [],
     board: [],
-    emptySpot: [],
+    size: 3,
+    occupiedSpot: [],
+    singlePlayer: false,
 
     initPlayer: function(id, name, symbol) {
         if (this.players.length < 2) {
@@ -22,6 +24,14 @@ const ttt = {
         }
 
         if (this.players.length === 2) {
+            if (this.players[0]['name'] === 'AI') {
+                this.players[0]['id'] = '3';
+                this.singlePlayer = true;
+            } else if (this.players[1]['name'] ==='AI') {
+                this.players[1]['id'] = '3';
+                this.singlePlayer = true;
+            }
+
             if (this.players[0]['id'] > this.players[1]['id']) {
                 this.players = this.players.reverse()
             }
@@ -41,16 +51,18 @@ const ttt = {
     },
 
     initBoard: function(size=3) {
+        this.size = size;
         if(!this.board.length) {
             for (let i = 0; i < size; i++) {
                 this.board.push([]);
                 for (let j = 0; j < size; j++) {
                     this.board[i].push("");
-                    this.emptySpot.push([i,j])
                 }
             }
         }
         localStorage.setItem('ttt.board', JSON.stringify(this.board));
+        this.genEmptySpots();
+        this.genWinCombo();
         return this.board;
     },
 
@@ -109,19 +121,17 @@ const ttt = {
     },
 
     makeMove: function(playerIndex, row, column) {
+        console.log(this.players[playerIndex]);
         const symbol = this.players[playerIndex].symbol;
+
         if (!this.board[row][column]) {
+
             this.board[row][column] = symbol;
             localStorage.setItem('ttt.board', JSON.stringify(this.board));
             console.log(`${this.players[playerIndex].name} takes move`);
-            let i =0;
-            for (i; i<this.emptySpot.length; i++) {
-                if (this.emptySpot[i][0] === row && this.emptySpot[i][1] === column) {
-                    break;
-                }
-            };
-            this.emptySpot.splice(i, 1);
-            localStorage.setItem('ttt.emptySpot', JSON.stringify(this.emptySpot));
+            this.updateEmptySpots(row, column);
+            this.occupiedSpot.push([row, column]);
+
             return true
         }
         console.log(`Position has been taken.`);
@@ -133,23 +143,154 @@ const ttt = {
         if (this.horizontal(symbol)||this.vertical(symbol)||this.diagonalI(symbol)||this.diagonalII(symbol)) {
             this.players[currentPlayerIndex]['winCounter'] +=1;
             localStorage.setItem('ttt.players', JSON.stringify(this.players))
-;            return true;
+            ;            return true;
         }
     },
 
     reset: function() {
+        this.occupiedSpot=[];
         this.board= [];
-        this.emptySpot= [];
+        this.emptySpots= [];
         localStorage.setItem('ttt.board', JSON.stringify(this.board));
         localStorage.setItem('ttt.emptySpot', JSON.stringify(this.emptySpot));
-    }
+    },
 
-    //     aiMove: function() {
-    //         if () {
-    //
-    //         }
-    //
-    // },
+    winCombo: [],
+    genWinCombo: function (){
+        this.winCombo =[];
+        let lid1 = [];
+        let lid2 = [];
+        for (let i = 0; i < this.size; i++) {
+            let lih = [];
+            let liv = [];
+
+            lid1.push([i,i])
+            lid2.push([this.size-1-i, i])
+            for (let j = 0; j < this.size; j++){
+                lih.push([i, j])
+                liv.push([j, i])
+            }
+            this.winCombo.push(lih);
+            this.winCombo.push(liv);
+        }
+        this.winCombo.push(lid1);
+        this.winCombo.push(lid2);
+        return this.winCombo;
+    },
+
+    emptySpots:[],
+    emptySpotsMediumLevel: [],
+    emptySpotsHardLevel: [],
+
+    genEmptySpots: function() {
+        this.emptySpots =[];
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                this.emptySpots.push([i, j]);
+            }
+        }
+    },
+
+    updateEmptySpots: function(row, column) {
+        let i = 0;
+        for (i; i<this.emptySpots.length; ++i) {
+            if (this.emptySpots[i][0] == row && this.emptySpots[i][1]  == column) {
+                break
+            }
+        }
+        this.emptySpots.splice(i, 1)
+    },
+
+    aiRandomMove: function() {
+        return this.emptySpots[Math.floor(Math.random()* this.emptySpots.length)]
+    },
+
+    aiEasy: function() {
+        this.emptySpotsMediumLevel =[];
+        if (this.occupiedSpot.length<2) {
+            return this.aiRandomMove();
+        } else {
+            let lastMove = this.occupiedSpot[this.occupiedSpot.length-1];
+            console.log(lastMove);
+            for (let i of this.emptySpots) {
+                if (Math.abs(i[0]-Number(lastMove[0]))<=1 && Math.abs(i[1]-Number(lastMove[1])) <=1) {
+                    this.emptySpotsMediumLevel.push(i);
+                }
+            }
+        }
+        let pos = this.emptySpotsMediumLevel[Math.floor(Math.random()* this.emptySpotsMediumLevel.length)]
+        return pos? pos: this.aiRandomMove();
+    },
+
+    aiHard: function() {
+
+        const ai = this.players[1]
+        const player = this.players[0];
+        let filteredCombo = [];
+        for (let combo of this.winCombo) {
+            for (let pos of combo) {
+                if (this.board[pos[0]][pos[1]] === ai['symbol']) {
+                    filteredCombo.push(combo);
+                    break;
+                }
+            }
+        }
+
+        let defendCombo =[];
+        let filteredComboLength = filteredCombo.length;
+        for (let combo1 of this.winCombo) {
+            for (let i in filteredCombo){
+                if (JSON.stringify(combo1) === JSON.stringify(filteredCombo[i])) {
+                    break;
+                } else if (i == filteredComboLength-1) {
+                    defendCombo.push(combo1);
+                }
+            }
+        }
+
+
+        let toFindCommonSpot = [];
+        for (let combo of defendCombo) {
+            let emptySpot = [];
+            for (let i = 0; i < combo.length; i++) {
+                if (this.board[combo[i][0]][combo[i][1]] === player.symbol) {
+                } else {
+                    emptySpot.push(combo[i])
+                }
+            }
+            if (emptySpot.length === 1) {
+                return emptySpot[0];
+            } else if (emptySpot.length === 2) {
+                toFindCommonSpot.push(emptySpot);
+            }
+        }
+
+        for (let i =0; i < toFindCommonSpot.length-1; i++) {
+            for (let j = i+1; j < toFindCommonSpot.length; j++) {
+                for (let id = 0; id < 2; id++){
+                    for (let jd = 0; jd <2; jd++) {
+                        if (JSON.stringify(toFindCommonSpot[i][id])===JSON.stringify(toFindCommonSpot[j][jd])) {
+                            return toFindCommonSpot[i][id]
+                        }
+                    }
+                }
+            }
+        }
+        return  this.aiEasy();
+    },
+
+    aiEasyMove: function(playerIndex) {
+        const pos = this.aiEasy();
+        this.makeMove(playerIndex, String(pos[0]), String(pos[1]));
+        return pos;
+    },
+
+    aiHardMove: function(playerIndex) {
+        const pos = this.aiHard();
+        this.makeMove(playerIndex, String(pos[0]), String(pos[1]));
+        console.log(pos);
+        return pos;
+}
 }
 
 
